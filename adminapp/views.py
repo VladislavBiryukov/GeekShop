2ebcc5979c93b443d9e11cdb3b819fc91a6780b1
Vlad <b.vlad87@mail.ru>
2022-01-27 11:58:17 +0300

    lesson_7. Admin link and firewall.

--- adminapp/views.py
+++ adminapp/views.py
@@ -1,3 +1,87 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.contrib.auth.decorators import user_passes_test
+from django.shortcuts import get_object_or_404, redirect, render
 
-# Create your views here.
+from authnapp.models import ShopUser
+from mainapp.models import Product, ProductCategory
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def admin_main(request):
+    response = redirect("admin:users")
+    return response
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def users(request):
+    title = "админка/пользователи"
+    users_list = ShopUser.objects.all().order_by("-is_active", "-is_superuser", "-is_staff", "username")
+    content = {"title": title, "objects": users_list, "media_url": settings.MEDIA_URL}
+    return render(request, "adminapp/users.html", content)
+
+
+def user_create(request):
+    response = redirect("admin:users")
+    return response
+
+
+def user_update(request, pk):
+    response = redirect("admin:users")
+    return response
+
+
+def user_delete(request, pk):
+    response = redirect("admin:users")
+    return response
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def categories(request):
+    title = "админка/категории"
+    categories_list = ProductCategory.objects.all()
+    content = {"title": title, "objects": categories_list, "media_url": settings.MEDIA_URL}
+    return render(request, "adminapp/categories.html", content)
+
+
+def category_create(request):
+    response = redirect("admin:categories")
+    return response
+
+
+def category_update(request, pk):
+    response = redirect("admin:categories")
+    return response
+
+
+def category_delete(request, pk):
+    response = redirect("admin:categories")
+    return response
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def products(request, pk):
+    title = "админка/продукт"
+    category = get_object_or_404(ProductCategory, pk=pk)
+    products_list = Product.objects.filter(category__pk=pk).order_by("name")
+    content = {"title": title, "category": category, "objects": products_list, "media_url": settings.MEDIA_URL}
+    return render(request, "adminapp/products.html", content)
+
+
+def product_create(request, pk):
+    response = redirect("admin:categories")
+    return response
+
+
+def product_read(request, pk):
+    response = redirect("admin:categories")
+    return response
+
+
+def product_update(request, pk):
+    response = redirect("admin:categories")
+    return response
+
+
+def product_delete(request, pk):
+    response = redirect("admin:categories")
+    return response
