c47e8c940a4d3c4b11f710d63326c7b7cdfdf1be
Vlad <b.vlad87@mail.ru>
2022-01-27 11:25:18 +0300

    Lesson 6. Login fix

--- basketapp/views.py
+++ basketapp/views.py
@@ -1,15 +1,20 @@
+from django.conf import settings
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import HttpResponseRedirect, get_object_or_404, render
 
 from basketapp.models import Basket
 from mainapp.models import Product
 
 
+@login_required
 def basket(request):
-    # content = {}
-    # return render(request, "basketapp/basket.html", content)
-    return HttpResponseRedirect(request.META.get("HTTP_REFERER"))
+    title = "корзина"
+    basket_items = Basket.objects.filter(user=request.user).order_by("product__category")
+    content = {"title": title, "basket_items": basket_items, "media_url": settings.MEDIA_URL}
+    return render(request, "basketapp/basket.html", content)
 
 
+@login_required
 def basket_add(request, pk):
     product = get_object_or_404(Product, pk=pk)
     basket = Basket.objects.filter(user=request.user, product=product).first()
@@ -23,7 +28,8 @@ def basket_add(request, pk):
     return HttpResponseRedirect(request.META.get("HTTP_REFERER"))
 
 
-def basket_remove(request):
-    # content = {}
-    # return render(request, "basketapp/basket.html", content)
+@login_required
+def basket_remove(request, pk):
+    basket_record = get_object_or_404(Basket, pk=pk)
+    basket_record.delete()
     return HttpResponseRedirect(request.META.get("HTTP_REFERER"))
