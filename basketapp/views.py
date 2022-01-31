14aee598a7451f10a2c604640d9a8fe86fe20bee
VladislavBiryukov <74416089+VladislavBiryukov@users.noreply.github.com>
2022-01-31 10:09:37 +0300

    Lesson 6 (#6)
    * Lesson 6. Basket extension
    
    * Lesson 6. Hot product
    
    * Lesson 6. Basket read and delete
    
    * Lesson 6. Product page
    
    * Lesson 6. Login fix
    
    * Lesson 6. After login
    
    * Lesson 6. AJAX
    
    * Lesson 6. Code style fix

--- basketapp/views.py
+++ basketapp/views.py
@@ -1,16 +1,7 @@
-from django.shortcuts import HttpResponseRedirect, get_object_or_404, render
-
 from basketapp.models import Basket
 from mainapp.models import Product
 
 
-def basket(request):
-    # content = {}
-    # return render(request, "basketapp/basket.html", content)
-    return HttpResponseRedirect(request.META.get("HTTP_REFERER"))
-
-
-def basket_add(request, pk):
     product = get_object_or_404(Product, pk=pk)
     basket = Basket.objects.filter(user=request.user, product=product).first()
 
@@ -23,7 +14,4 @@ def basket_add(request, pk):
     return HttpResponseRedirect(request.META.get("HTTP_REFERER"))
 
 
-def basket_remove(request):
-    # content = {}
-    # return render(request, "basketapp/basket.html", content)
     return HttpResponseRedirect(request.META.get("HTTP_REFERER"))
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,6 +1,3 @@
-from django.conf import settings
-from django.shortcuts import get_object_or_404, render
-from django.utils import timezone
 
 from basketapp.models import Basket
 
@@ -15,45 +12,12 @@ def main(request):
     content = {"title": title, "products": products, "media_url": settings.MEDIA_URL}
     return render(request, "mainapp/index.html", content)
 
-
-def products(request, pk=None):
-    title = "продукты"
-    links_menu = ProductCategory.objects.all()
-
-    basket = []
-    if request.user.is_authenticated:
-        basket = Basket.objects.filter(user=request.user)
-        # or you can use this
-        # _basket = request.user.basket.all()
-        # print(f'basket / _basket: {len(_basket)} / {len(basket)}')
-
-    if pk is not None:
-        if pk == 0:
-            products = Product.objects.all().order_by("price")
-            category = {"name": "все"}
-        else:
-            category = get_object_or_404(ProductCategory, pk=pk)
-            products = Product.objects.filter(category__pk=pk).order_by("price")
-        content = {
-            "title": title,
-            "links_menu": links_menu,
-            "category": category,
-            "products": products,
-            "media_url": settings.MEDIA_URL,
-            "basket": basket,
-        }
-        return render(request, "mainapp/products_list.html", content)
-    same_products = Product.objects.all()
     content = {
         "title": title,
         "links_menu": links_menu,
         "same_products": same_products,
         "media_url": settings.MEDIA_URL,
-        "same_products": same_products,
-        "basket": basket,
-    }
-    if pk:
-        print(f"User select category: {pk}")
+
     return render(request, "mainapp/products.html", content)
 
 
@@ -63,3 +27,15 @@ def contact(request):
     locations = Contact.objects.all()
     content = {"title": title, "visit_date": visit_date, "locations": locations}
     return render(request, "mainapp/contact.html", content)
+
+
+def product(request, pk):
+    title = "продукты"
+    content = {
+        "title": title,
+        "links_menu": ProductCategory.objects.all(),
+        "product": get_object_or_404(Product, pk=pk),
+        "basket": get_basket(request.user),
+        "media_url": settings.MEDIA_URL,
+    }
+    return render(request, "mainapp/product.html", content)
